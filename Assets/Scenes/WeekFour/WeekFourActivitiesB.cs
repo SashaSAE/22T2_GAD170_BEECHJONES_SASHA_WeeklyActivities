c5cf4f4108f5025414d73db5422518e526462caf
SashaSAE <107592269+SashaSAE@users.noreply.github.com>
2022-06-23 12:06:46 +1000

    Week 4 activities
    did a pair programming act

--- Assets/Scenes/WeekFour/WeekFourActivitiesB.cs
+++ Assets/Scenes/WeekFour/WeekFourActivitiesB.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class 2WeekFourActivities : MonoBehaviour
+public class WeekFourActivitiesB : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
